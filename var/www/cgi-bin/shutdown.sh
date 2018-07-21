fcf9b1c58012f84919f1bbb8c0fd7d82785d0fa6
Ray <whatdoineed2do@no.where.com>
2018-07-21 18:51:57 +0100

    only shutdown if on local network

--- var/www/cgi-bin/shutdown.sh
+++ var/www/cgi-bin/shutdown.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
+
+REQ_ORIGIN=$(echo $REMOTE_ADDR | cut -f 1,2 -d '.')
+
+if [ "$REQ_ORIGIN" = "192.168" ]; then
 cat << EOF
 Content-Type: text/plain
 
 System going down $(date)
 $(sudo /sbin/shutdown -h -t 1 now "web i/f requested shutdown")
 EOF
+else
+cat << EOF
+Content-Type: text/plain
 
+System stable
+EOF
+fi
